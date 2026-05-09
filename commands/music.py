876c45a72d1bfa0b68580e0bd856971c533adb37
Rizdian <rizdian.dinata@gmail.com>
2026-05-09 17:20:52 +0700

    fix music.py

--- commands/music.py
+++ commands/music.py
@@ -31,6 +31,12 @@ ytdl_format_options = {
     "default_search": "ytsearch",
     "cookiefile": COOKIE_PATH,
     "extract_flat": False,
+    "nocheckcertificate": True,
+    "ignoreerrors": False,
+    "logtostderr": False,
+    "add_header": [
+        "User-Agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
+    ],
 }
 
 ffmpeg_options = {
